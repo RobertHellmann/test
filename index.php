5148a39d18e700b4c6d2fd87dcaa4cd9cf48e0e4
Robert Hellmann <r.hellmann@outlook.com>
2015-07-20 21:18:09 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>.itGroup</title>
+
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,700,700italic">
+		<link rel="stylesheet" href="styles/styles.css">
+
+
+	</head>
+	<body>
+
+
+
+		<div class="logo logo-main">
+			<img src="">
+		</div>	
+	
+		<div class="navigation navigation-main">
+			<nav class="menu menu-main">
+				<ul class="nav nav-tabs">
+				  <li role="presentation"><a href="#">Home</a></li>
+				  <li role="presentation"><a href="#">Über Uns</a></li>
+				  <li role="presentation"><a href="#">Services</a></li>
+				  <li role="presentation"><a href="#">Maps</a></li>
+				</ul>
+			</nav>
+		</div>
+
+		
+    	
+
+
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	</body>
+</html>
